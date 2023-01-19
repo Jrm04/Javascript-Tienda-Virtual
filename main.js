57b79c4ea0c30e5b1b25092973e1416268ce2331
Jehiko Rashid Mosquera Mosquera <jeramos04@hotmail.com>
2023-01-19 14:55:48 -0500

    avances 19 de Enero

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const carrito = document.querySelector('.navbar-shopping-card');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
 
 /*
 navEmail.addEventListener('click', toggleDesktopMenu);
@@ -61,4 +63,81 @@ menú y podamos ver el carrito
 
     aside.classList.toggle('inactive');
 });
+
+const productsList = [];
+productsList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+
+productsList.push({
+    name: 'Computadora',
+    price: 12000,
+    image: 'https://images.pexels.com/photos/3042508/pexels-photo-3042508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+
+productsList.push({
+    name: 'Silla',
+    price: 20,
+    image: 'https://images.pexels.com/photos/2180883/pexels-photo-2180883.jpeg'
+});
+
+
+function renderProducts(arr){
+    for (product of arr){
+        const productCard = `<div class="product-card">
+                    <img src="${product.image}" alt="">
+                    <div class="product-info">
+                        <div>
+                            <p>$ ${product.price}</p>
+                            <p>${product.name}</p>        
+                        </div>
+                        <figure>
+                            <img src="./Platzi_YardSale_Icons/bt_add_to_cart.svg" alt="">
+                        </figure>
+                    </div>
+                </div>`;
+        
+        cardsContainer.innerHTML += productCard;
+        /* const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        const productInfoFigure = document.createElement('figure');
+    
+        const imgCarrito = document.createElement('img');
+        imgCarrito.setAttribute('src', './Platzi_YardSale_Icons/bt_add_to_cart.svg');
+    
+    
+        //Insertar los elementos hios dentro de los elementos padres
+    
+        productInfoFigure.appendChild(imgCarrito);
+    
+        productInfoDiv.append(productPrice, productName);
+    
+        productInfo.append(productInfoDiv, productInfoFigure);
+    
+        productCard.append(productImg, productInfo);
+    
+        cardsContainer.appendChild(productCard) */
+    
+    }
+}
+
+renderProducts(productsList);
+                   
 
