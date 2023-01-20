066b4cf89706372bcf25ec65ac581ba015338b84
Jehiko Mosquera <jeramos04@hotmail.com>
2023-01-19 20:35:39 -0500

    vista detalles del producto agregada

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const burguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const carrito = document.querySelector('.navbar-shopping-card');
-const aside = document.querySelector('.product-detail');
+const shoppingCardContainer = document.querySelector('#shoppingCardContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 
@@ -17,10 +17,10 @@ navEmail.addEventListener('click', toggleDesktopMenu);
 
 navEmail.addEventListener('click', () => {
 
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isshoppingCardContainerClosed = shoppingCardContainer.classList.contains('inactive');
 
-    if(!isAsideClosed){
-        aside.classList.add('inactive');
+    if(!isshoppingCardContainerClosed){
+        shoppingCardContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
@@ -34,10 +34,10 @@ burguerMenu.addEventListener('click', () => {
     en caso de que si esté añadida significa que el carrito está desaparecido por lo que podremos mostrar el menú sin problemas,
     caso contrario si el carrito está abierto, por lo cual si queremos ver ell menú tenemos que añadir la clase inactive */
 
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isshoppingCardContainerClosed = shoppingCardContainer.classList.contains('inactive');
 
-    if(!isAsideClosed){
-        aside.classList.add('inactive');
+    if(!isshoppingCardContainerClosed){
+        shoppingCardContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
@@ -61,10 +61,29 @@ menú y podamos ver el carrito
         desktopMenu.classList.add('inactive');
     }
 
-    aside.classList.toggle('inactive');
+    shoppingCardContainer.classList.toggle('inactive');
 });
 
 const productsList = [];
+
+productsList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+
+productsList.push({
+    name: 'Computadora',
+    price: 12000,
+    image: 'https://images.pexels.com/photos/3042508/pexels-photo-3042508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+
+productsList.push({
+    name: 'Silla',
+    price: 20,
+    image: 'https://images.pexels.com/photos/2180883/pexels-photo-2180883.jpeg'
+});
+
 productsList.push({
     name: 'Bike',
     price: 120,
@@ -84,13 +103,15 @@ productsList.push({
 });
 
 
+
+
 function renderProducts(arr){
     for (product of arr){
         const productCard = `<div class="product-card">
                     <img src="${product.image}" alt="">
                     <div class="product-info">
                         <div>
-                            <p>$ ${product.price}</p>
+                            <p> $ ${product.price}</p>
                             <p>${product.name}</p>        
                         </div>
                         <figure>
@@ -137,7 +158,6 @@ function renderProducts(arr){
     
     }
 }
-
 renderProducts(productsList);
                    
 
